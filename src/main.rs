3aebc3adb6fae396d8a3209effd7bd4f0f47dd41
Fabrice CROISEAUX <fcroiseaux@gmail.com>
2021-01-24 19:39:00 +0100

    Test cals

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ async fn ics_merge(req: Request<Body>) -> Result<Response<Body>, Infallible> {
         }
         (&Method::GET, "/fxzo.ics") => {
             let calendars = [
-                "https://webmail.intech.lu/owa/calendar/25be2d37664e47899a9c952e5d652e98@Intech.lu/497fedb4dfb34173a0770b0879cbacbc17006863668209442216/calendar.ics",
-                "https://calendar.google.com/calendar/ical/fcroiseaux%40gmail.com/private-db183d5c924a98df943d4ed104fbb95f/basic.ics",
+           //     "https://webmail.intech.lu/owa/calendar/25be2d37664e47899a9c952e5d652e98@Intech.lu/497fedb4dfb34173a0770b0879cbacbc17006863668209442216/calendar.ics",
+           //     "https://calendar.google.com/calendar/ical/fcroiseaux%40gmail.com/private-db183d5c924a98df943d4ed104fbb95f/basic.ics",
                 "https://calendar.google.com/calendar/u/1?cid=ZmFicmljZUB0b2tlbnkuY29t",
                 "https://outlook.office365.com/owa/calendar/4e690d4c256b4fca9a11d2c03328a21c@lumena.tech/04e70dc6d07c4e6c8c01377ebdab5c6f9379776718195930947/calendar.ics"
             ];
